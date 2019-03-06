24c21eef985b7d0d5372eb1e01c65a7305c5f05e
frozen521 <yanni31316@outlook.com>
2019-03-06 10:47:01 +0800

    first tutor

---
+++ jsfile/obj.js
@@ -0,0 +1,14 @@
+let web={
+    _age:0,
+    // set startyear(startyear) {
+    //   if (startyear<=new Date().getFullYear()) {
+    //     this._age=new Date().getFullYear()-startyear;
+    //
+    //   }
+    // },
+    // get age(){
+    //     return this._age;
+    // }
+};
+web.startyear=2015;
+console.log(web.age)
