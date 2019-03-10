12d8c280a6d2e14a7aae531afde4f66be65dbabc
frozen521 <yanni31316@outlook.com>
2019-03-10 22:06:14 +0800

    :bento: Adding yarn

--- webpack.dev.js
+++ webpack.dev.js
@@ -43,79 +43,80 @@ const precss = require('precss');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                include: [path.resolve(__dirname, 'src')],
+                loader: 'babel-loader',
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+                options: {
+                    plugins: ['syntax-dynamic-import'],
 
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                modules: false
+                            }
+                        ]
+                    ]
+                },
 
-				test: /\.js$/
-			},
-			{
-				test: /\.css$/,
+                test: /\.js$/
+            },
+            {
+                test: /\.css$/,
 
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
 
-						options: {
-							importLoaders: 1,
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'postcss-loader',
+                        options: {
+                            importLoaders: 1,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
 
-						options: {
-							plugins: function() {
-								return [precss, autoprefixer];
-							}
-						}
-					}
-				]
-			}
-		]
-	},
+                        options: {
+                            plugins: function () {
+                                return [precss, autoprefixer];
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
 
-	entry: {
-		app: './build/app'
-	},
+    entry: './src/index.js'
+    ,
 
-	output: {
-		filename: '[name].[chunkhash].js'
-	},
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname,'dist')
+    },
 
-	mode: 'development',
+  
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/
+                }
+            },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true
+        }
+    }
 };
