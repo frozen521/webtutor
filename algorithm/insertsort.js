95e2189a538c79739bc01e54932b925804205db1
frozen521 <yanni31316@outlook.com>
2019-03-15 12:38:02 +0800

    :bento: Adding algorithm

--- algorithm/insertsort.js
+++ algorithm/insertsort.js
@@ -1,9 +1,9 @@
 function insertionSort(array) {
     if (Object.prototype.toString.call(array).slice(8, -1) === 'Array') {
         console.time('插入排序耗时：');
-        for (var i = 1; i < array.length; i++) {
-            var key = array[i];
-            var j = i - 1;
+        for (let i = 1; i < array.length; i++) {
+            const key = array[i];
+            let j = i - 1;
             while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j--;
@@ -18,5 +18,6 @@ function insertionSort(array) {
         return 'array is not an Array!';
     }
 }
-var m=insertionSort([3,4,5,6,7,663,42,33,3,894,2,74,8,34,8,3])
+
+const m = insertionSort([3, 4, 5, 6, 7, 663, 42, 33, 3, 894, 2, 74, 8, 34, 8, 3]);
 console.log(m)
