2a7bdf6dbb615283d4dec7c4701c5be9290c0fcf
copyer1998 <2782644531@qq.com>
2019-03-27 23:18:17 +0800

    :bento: Adding algorithm

--- es6/index.js
+++ es6/index.js
@@ -1,22 +1,13 @@
-class Car {
-    constructor(brand, lineup) {
-        this.brand = brand
-    }
-
-     show() {
-        console.log(`this is ${this.brand}`)
-    }
-}
-class Lexus  extends Car{
-    constructor(brand,lineup){
-        super(brand)
-        this.lineup=lineup;
-    }
-    getPrice(){
-        console.log('first step')
+let user={
+    name:'john',
+    sex:'man',
+    get username(){
+        return this.name;
+    },
+    set username(name){
+        this.name=name;
     }
 }
-
-let mycar=new Lexus('lexu','rx');
-mycar.show();
-mycar.getPrice()
+console.log(user.name);
+user.name='hlllll'
+console.log(user.name );
