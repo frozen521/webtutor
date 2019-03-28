b20371a145bbfff72a4ab17deb33f73511c55dd9
sven <2284369344@qq.com>
2019-03-28 14:56:33 +0800

    add new

--- es6/arraytest/index01.js
+++ es6/arraytest/index01.js
@@ -39,4 +39,48 @@ const go = function*() {
 }
 {
     console.log("________________________________________________");
+    console.log(Array.from([1, 3, 4], x => x * x));
+}
+
+{
+    console.log("________________________________________________");
+    function typefo() {
+        return Array.from(arguments, value => typeof value); //返回数据类型
+    }
+}
+{
+    console.log("________________________________________________");
+    let nameset = Array.of(2, 3, 4, 5, 6); //[2,3,4,5,6]
+    console.log(nameset);
+}
+{
+    console.log("________________________________________________");
+    let ii = [1, 2, 3, 4, 5].copyWithin(0, 3, 4);
+    console.log(ii);
+}
+{
+    console.log("________________________________________________");
+    function fv(v) {
+        return v > this.age;
+    }
+    let person = { name: "json", age: 20 };
+    let resp = [12, 45, 23, 11].find(fv, person);
+    console.log(resp);
+}
+{
+    console.log("________________________________________________");
+    const a = ["q", "b", "c"].fill(3);
+    console.log(a);
+    let arr = new Array(3).fill({ name: "mike" });
+    arr[0].name = "ben";
+    console.log(arr); //赋值的是地址
+}
+{
+    console.log("________________________________________________");
+    let a = [1, 3, [34, 44]].flat();
+    console.log(a);
+    let m = [2, 3, 4].flatMap(x => [x, x * 2]);
+    console.log(m);
+    let n = [3, 4, 5].flatMap(x => [[x * 2]]);
+    console.log(n);
 }
