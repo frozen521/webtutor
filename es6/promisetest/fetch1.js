fd617a9e588fb937579697978fcc8f557bbf5460
copyer1998 <2782644531@qq.com>
2019-03-28 13:41:37 +0800

    :bento: Adding algorithm

--- es6/promisetest/fetch1.js
+++ es6/promisetest/fetch1.js
@@ -1,9 +1,5 @@
-function timeout(ms ) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve,ms,'done')
-  })
-  ;
-}
-timeout(100).then((value) => {
-  console.log(value)
-});
+let person = new class {
+    constructor(name) {
+        this.name = name;
+    }
+}();
