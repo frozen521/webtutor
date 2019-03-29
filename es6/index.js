4204664cc996d169fd5bc498868dd7e0022af236
sven <2284369344@qq.com>
2019-03-29 12:38:15 +0800

    1
    1

--- es6/index.js
+++ es6/index.js
@@ -1,13 +1,13 @@
-let user={
-    name:'john',
-    sex:'man',
-    get username(){
+let user = {
+    name: "john",
+    sex: "man",
+    get username() {
         return this.name;
     },
-    set username(name){
-        this.name=name;
+    set username(name) {
+        this.name = name;
     }
-}
+};
 console.log(user.name);
-user.name='hlllll'
-console.log(user.name );
+user.name = "hlllll";
+console.log(user.name);
